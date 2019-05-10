20bdf11dc93e79f831428cac1af694c5fd4fde1b
unknown <lvguanghui@xiaoniu.com>
2019-05-10 13:27:21 +0800

    spring-boot-helloWorld

---
+++ spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldMessage.java
@@ -0,0 +1,63 @@
+package com.neo.controller;
+
+public class HelloWorldMessage {
+    private String scope;
+
+    private String name;
+
+    private String id0;
+
+    private String id1;
+
+    private String alarmMessage;
+
+    private String startTime;
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId0() {
+        return id0;
+    }
+
+    public void setId0(String id0) {
+        this.id0 = id0;
+    }
+
+    public String getId1() {
+        return id1;
+    }
+
+    public void setId1(String id1) {
+        this.id1 = id1;
+    }
+
+    public String getAlarmMessage() {
+        return alarmMessage;
+    }
+
+    public void setAlarmMessage(String alarmMessage) {
+        this.alarmMessage=alarmMessage;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+}
